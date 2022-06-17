f928736011bec879ab7485b8a07da8eacac0acc9
lorex <lorex59@mail.ru>
2022-06-17 18:44:57 +1000

    add controllers and routes

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import  express from "express"
+import mongoose from "mongoose"
 
 const PORT = 5000
 
+const DB_URL = "mongodb+srv://lorex59:25092003Danil@project.lfdeyhb.mongodb.net/project?retryWrites=true&w=majority"
+
 const app = express()
 
+async function startApp(uri, callback){
+    try {
+        await mongoose.connect(DB_URL,{useNewUrlParser: true, useUnifiedTopology: true})
+        app.listen(PORT, () => console.log("Server start on port" + PORT))
+    }catch (e) {
+        console.log(e)
+    }
+}
 
-app.listen(PORT, () => console.log("Server work" + PORT))
-
+startApp()
